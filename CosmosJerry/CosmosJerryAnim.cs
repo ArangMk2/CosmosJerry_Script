1758bce80b1431c1863f155170d577304179bbf5
Arang_Mk2 <87531267+ArangMk2@users.noreply.github.com>
2024-05-01 12:34:29 +0900

    不要なusingの削除

--- CosmosJerry/CosmosJerryAnim.cs
+++ CosmosJerry/CosmosJerryAnim.cs
@@ -1,10 +1,5 @@
 ﻿using Spine;
 using Spine.Unity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CosmosJerry
 {
